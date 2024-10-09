5ca3210385ffb6abc24755747babf392a6667001
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2024-10-09 16:18:59 +0000

    add  and  files

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Oi Juu, eu te amo demais. Tu é o amor da minha vida toda!</h1>
+function Home() {
+  return <h1>Oi Juu, eu te amo demais. Tu é o amor da minha vida toda!</h1>;
+}
+
+function teste() {
+  return;
 }
 
 export default Home;
