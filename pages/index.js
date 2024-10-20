5b0b52c88f053fcaf013468a01f250182da2d815
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2024-10-20 23:37:58 +0000

    add gif on `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 function Home() {
   return (
-    <h1 style={{ textAlign: "center" }}>
-      Oi Juu, eu te amo demais. Tu é o amor da minha vida toda!
-    </h1>
+    <>
+      <div style={{ display: "grid", justifyItems: "center" }}>
+        <h1 style={{ textAlign: "center" }}>
+          Oi Juu, eu te amo demais. Tu é o amor da minha vida toda! ❤️❤️❤️
+        </h1>
+        <iframe
+          src={"https://giphy.com/embed/9G0AdBbVrkV3O"}
+          alt="teste..."
+          style={{ border: "none", width: "720px", height: "60vh" }}
+        ></iframe>
+      </div>
+    </>
   );
 }
 
