f3701dca3879fec483f16df89857851c838b4295
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2024-11-02 01:44:04 +0000

    update and add to the `\status` infos of postgress

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,37 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const res = await database.query("SELECT 1+1 as sum;");
-  const resp = res.rows;
+  const dateNow = new Date().toISOString();
 
-  console.log(resp);
+  //Faz a query e recebe as informações de versão
+  const databaseVersionResponse = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResponse.rows[0].server_version;
 
-  response.status(200).json({ chave: "teste éé", resp });
+  //Faz a query e recebe as informações de max_connections depois de tratar
+  const databaseMaxConnectionsResponse = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResponse.rows[0].max_connections;
+
+  //Faz a query e recebe as informações de connections ativas
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpendConnectionsResponse = await database.query({
+    text: "SELECT count(*)::int from pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const userConnectionsRows = databaseOpendConnectionsResponse.rows[0].count;
+
+  response.status(200).json({
+    updated_at: dateNow,
+    dependecies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opend_connections: userConnectionsRows,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/v1/status/get.test.js
+++ tests/integration/v1/status/get.test.js
@@ -1,4 +1,40 @@
 test("Get should be 200 on api/v1/status", async () => {
+  // Verifica se a aplicação está viva
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+});
+
+test("Verify updated_at valid format", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  //Verifica se o update_at está sendo preenchido
+  expect(responseBody.updated_at).toBeDefined();
+
+  //Verifica a fundo se o valor informado na chave update_at é realmente válido
+  //Converte a entrada para ISO novamente, se a entrada for diferente de uma data válida o teste quebra
+  const parsedDateNow = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedDateNow);
+});
+
+test("Verify database integrity", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  //Verifica se o database está sendo preenchido
+  expect(responseBody.dependecies.database.version).toBeDefined();
+  expect(responseBody.dependecies.database.max_connections).toBeDefined();
+  expect(responseBody.dependecies.database.opend_connections).toBeDefined();
+
+  const valueVersion = responseBody.dependecies.database.version;
+  expect(valueVersion).toEqual("16.0");
+
+  const valueMaxConnections = responseBody.dependecies.database.max_connections;
+  expect(valueMaxConnections).toEqual(100);
+
+  const valueUsedConnections =
+    responseBody.dependecies.database.opend_connections;
+  expect(valueUsedConnections).toEqual(1);
 });
