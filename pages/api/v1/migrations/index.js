33e276f445bf3a3de5368f72a0778d086657a1c5
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2025-01-22 16:16:29 +0000

    fix `\migrations` bug connection

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,42 +3,50 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-
-  const deafultMigrationsOptions = {
-    dbClient: dbClient,
-    dir: join("infra", "migrations"),
-    dryRun: false,
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
-  // Caso o metodo seja POST mostra e executa as migrações
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...deafultMigrationsOptions,
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "{$request.method}" not allowed`,
     });
+  }
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
 
-    await dbClient.end();
+    const deafultMigrationsOptions = {
+      dbClient: dbClient,
+      dir: join("infra", "migrations"),
+      dryRun: false,
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+    // Caso o metodo seja POST mostra e executa as migrações
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...deafultMigrationsOptions,
+      });
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
     }
-    return response.status(200).json(migratedMigrations);
-  }
 
-  // Caso for GET, mostra as migrações pendentes
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner({
-      ...deafultMigrationsOptions,
-      dryRun: true,
-    });
+    // Caso for GET, mostra as migrações pendentes
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner({
+        ...deafultMigrationsOptions,
+        dryRun: true,
+      });
 
+      return response.status(200).json(pendingMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
     await dbClient.end();
-
-    return response.status(200).json(pendingMigrations);
   }
-
-  return response.status(405).end();
 }
