3ca5d51fa75b413e28e5c718a33cd22b84eafcb6
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2025-04-06 23:59:23 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/v1/migrations/get.test.js
+++ tests/integration/v1/migrations/get.test.js
@@ -6,22 +6,26 @@ beforeAll(async () => {
   await database.query("DROP Schema public cascade; create schema public;");
 });
 
-test("GET should be 200 on api/v1/migrations", async () => {
-  // Verifica se a aplicação está viva
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations", async () => {
+      // Verifica se a aplicação está viva
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  //Se ta respondendo um array...
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-});
+      const responseBody = await response.json();
+      //Se ta respondendo um array...
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
 
-test("GET shouldn't be make a real migration", async () => {
-  const { rows: queryResponse } = await database.query(
-    "SELECT * FROM pgmigrations",
-  );
+    test("Shouldn't make a real migration", async () => {
+      const { rows: queryResponse } = await database.query(
+        "SELECT * FROM pgmigrations",
+      );
 
-  expect(Array.isArray(queryResponse)).toBe(true);
-  expect(queryResponse.length).toBe(0);
+      expect(Array.isArray(queryResponse)).toBe(true);
+      expect(queryResponse.length).toBe(0);
+    });
+  });
 });
--- tests/integration/v1/migrations/post.test.js
+++ tests/integration/v1/migrations/post.test.js
@@ -6,31 +6,45 @@ beforeAll(async () => {
   await database.query("DROP Schema public cascade; create schema public;");
 });
 
-test("POST should be 200 on api/v1/migrations", async () => {
-  // Verifica se a aplicação está viva
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        // Verifica se a aplicação está viva
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response1.status).toBe(201); // HTTP 201 = Created
+        const response1Body = await response1.json();
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        //Verifica se o segundo POST não quebra os testes
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+        const response2Body = await response2.json();
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
+
+      test("Should make a real migration", async () => {
+        const { rows: queryResponse } = await database.query(
+          "SELECT * FROM pgmigrations",
+        );
+        expect(Array.isArray(queryResponse)).toBe(true);
+        expect(queryResponse.length).toBeGreaterThan(0);
+      });
+    });
   });
-
-  expect(response1.status).toBe(201); // HTTP 201 = Created
-  const response1Body = await response1.json();
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
-
-  //Verifica se o segundo POST não quebra os testes
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-  const response2Body = await response2.json();
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
-});
-
-test("POST should be make a real migration", async () => {
-  const { rows: queryResponse } = await database.query(
-    "SELECT * FROM pgmigrations",
-  );
-  expect(Array.isArray(queryResponse)).toBe(true);
-  expect(queryResponse.length).toBeGreaterThan(0);
 });
