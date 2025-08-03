d4ffe5ed18da074debcfee1cb9739d6308edfb8e
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2025-08-03 13:35:04 +0000

    feat: create `user.findOneByUsername` method and use on endpoint `api/v1/users/[username]`

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -68,8 +68,38 @@ async function create(userInputValues) {
   }
 }
 
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+      LIMIT 
+        1
+      ;`,
+      values: [username],
+    });
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistemas.",
+        action: "Verifique se o username informado está digitado corretamente.",
+      });
+    }
+    return result.rows[0];
+  }
+}
+
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
